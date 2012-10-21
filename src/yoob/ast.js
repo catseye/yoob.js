7c3a78f0b3123ce63e7e1d95044970ed743b264a
catseye <cpressey@gmail.com>
2012-10-21 21:49:03 +0100

    Add REPL demo, improve AST and SexpParser.

--- src/yoob/ast.js
+++ src/yoob/ast.js
@@ -11,10 +11,17 @@ yoob.AST = function(type, value, children) {
   this.toString = function() {
     var s = this.type + "("
     if (this.value !== undefined) {
-      s += "'" + this.value + "' ";
+      s += "'" + this.value + "'";
     }
-    for (var i = 0; i < this.children.length; i++) {
-      s += this.children[i].toString() + " ";
+    if (this.children !== undefined && this.children.length > 0) {
+      s + " ";
+      for (var i = 0; i < this.children.length; i++) {
+        if (this.children[i] !== undefined) {
+          s += this.children[i].toString();
+          if (i < this.children.length - 1)
+            s += " ";
+        }
+      }
     }
     return s + ")";
   };
