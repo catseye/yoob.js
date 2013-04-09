2142783ebca77921320f3eceea44a53eae1451ec
catseye <cpressey@gmail.com>
2013-04-09 20:02:03 +0100

    Indent less badly.

--- src/yoob/line-input-buffer.js
+++ src/yoob/line-input-buffer.js
@@ -24,59 +24,59 @@ yoob.LineInputBuffer = function() {
 
         var me = this;
         listenObject.addEventListener('keyup', function(e) {
-          //alert('keyup:' + e.keyCode);
-          switch (e.keyCode) {
-            case 8:   /* Backspace */
-              if (me.terminal !== undefined) {
-                me.terminal.write('\b \b');
-              }
-              if (me.text.length > 0) {
-                me.text = me.text.substring(0, me.text.length-1);
-                if (me.onupdate !== undefined) {
-                  me.onupdate(me.text);
+            //alert('keyup:' + e.keyCode);
+            switch (e.keyCode) {
+              case 8:   /* Backspace */
+                if (me.terminal !== undefined) {
+                    me.terminal.write('\b \b');
                 }
-              }
-              e.cancelBubble = true;
-              break;
-            case 13:  /* Enter */
-              if (me.terminal !== undefined) {
-                me.terminal.write('\n');
-              }
-              me.text = me.text.substring(0, me.text.length);
-              if (me.onenter !== undefined) {
-                me.onenter(me.text);
-              }
-              me.text = "";
-              e.cancelBubble = true;
-              break;
-            case 38:  /* Up arrow */
-              break;
-            case 40:  /* Down arrow */
-              break;
-            case 37:  /* Left arrow */
-              break;
-            case 39:  /* Right arrow */
-              break;
-          }
+                if (me.text.length > 0) {
+                    me.text = me.text.substring(0, me.text.length-1);
+                    if (me.onupdate !== undefined) {
+                      me.onupdate(me.text);
+                    }
+                }
+                e.cancelBubble = true;
+                break;
+              case 13:  /* Enter */
+                if (me.terminal !== undefined) {
+                    me.terminal.write('\n');
+                }
+                me.text = me.text.substring(0, me.text.length);
+                if (me.onenter !== undefined) {
+                    me.onenter(me.text);
+                }
+                me.text = "";
+                e.cancelBubble = true;
+                break;
+              case 38:  /* Up arrow */
+                break;
+              case 40:  /* Down arrow */
+                break;
+              case 37:  /* Left arrow */
+                break;
+              case 39:  /* Right arrow */
+                break;
+            }
         }, true);
 
         /* TODO support on more browsers, with keyup */
         listenObject.addEventListener('keypress', function(e) {
-          if (e.charCode === 0) return;
-          //alert('keypress:' + e.charCode);
-          if (e.altKey) {
-            //alert(e.charCode);
-            return;
-          }
-          var chr = String.fromCharCode(e.charCode);
-          if (me.terminal !== undefined) {
-            me.terminal.write(chr);
-          }
-          me.text += chr;
-          if (me.onupdate !== undefined) {
-            me.onupdate(me.text);
-          }
-          e.cancelBubble = true;
+            if (e.charCode === 0) return;
+            //alert('keypress:' + e.charCode);
+            if (e.altKey) {
+                //alert(e.charCode);
+                return;
+            }
+            var chr = String.fromCharCode(e.charCode);
+            if (me.terminal !== undefined) {
+                me.terminal.write(chr);
+            }
+            me.text += chr;
+            if (me.onupdate !== undefined) {
+                me.onupdate(me.text);
+            }
+            e.cancelBubble = true;
         }, true);
 
         return this;
