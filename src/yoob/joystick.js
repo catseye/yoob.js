e3d005bba84947a5ba16c613143e15c94803da64
catseye <cpressey@gmail.com>
2013-04-29 19:55:55 +0100

    "Dramatically" improve yoob.Joystick.

--- src/yoob/joystick.js
+++ src/yoob/joystick.js
@@ -5,46 +5,88 @@
  */
 if (window.yoob === undefined) yoob = {};
 
+/*
+ * Emulates a joystick.  Be default, this is with the cursor keys and (either)
+ * control key as the fire button, but the keys can be reconfigured by changing
+ * values in the keyMap map, for example:
+ *
+ *     var joystick = new yoob.Joystick().init();
+ *     joystick.keyMap[32] = joystick.fire;
+ *     joystick.keyMap[17] = undefined;
+ *
+ * Control is used by default rather than space because, on all browsers I have
+ * tried on both Ubuntu (FF, Chrome) and Windows (FF, IE), down+right and
+ * up+left combinations both prevent the space key from registering.
+ *
+ * Some finesse is built into this design; for example, the user may hold
+ * down right, then additionally hold down left (and the stick will register
+ * no horizontal movement), then release right (and the stick will then
+ * register leftward movement.)
+ *
+ * The joystick may either be used in a polled mode (and if you are animating
+ * using setInterval or setTimeout or requestAnimationFrame, you may wish to
+ * use this method; simply poll the joystick's dx and dy and buttonPressed
+ * on each frame) or evented mode, by assigning an onchange method.  (see
+ * eg/joystick.html for an example)
+ */
 yoob.Joystick = function() {
     this.init = function() {
         this.dx = 0;
         this.dy = 0;
-        this.button = false;
+        this.leftPressed = false;
+        this.rightPressed = false;
+        this.upPressed = false;
+        this.downPressed = false;
+        this.buttonPressed = false;
+        this.keyMap = {
+            17: this.fire,
+            37: this.left,
+            38: this.up,
+            39: this.right,
+            40: this.down
+        };
+        this.onchange = undefined;
         return this;
     };
 
-    this.keyMap = {
-        32: 'fire',
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
     this.fire = function(obj, pressed) {
-        obj.button = pressed;
+        if (obj.buttonPressed === pressed) return;
+        obj.buttonPressed = pressed;
+        if (obj.onchange !== undefined) obj.onchange();
     };
 
     this.left = function(obj, pressed) {
-        obj.dx = pressed ? -1 : 0;
+        if (obj.leftPressed === pressed) return;
+        obj.leftPressed = pressed;
+        obj.dx = (obj.leftPressed ? -1 : 0) + (obj.rightPressed ? 1 : 0);
+        if (obj.onchange !== undefined) obj.onchange();
     };
 
     this.up = function(obj, pressed) {
-        obj.dy = pressed ? -1 : 0;
+        if (obj.upPressed === pressed) return;
+        obj.upPressed = pressed;
+        obj.dy = (obj.upPressed ? -1 : 0) + (obj.downPressed ? 1 : 0);
+        if (obj.onchange !== undefined) obj.onchange();
     };
 
     this.right = function(obj, pressed) {
-        obj.dx = pressed ? 1 : 0;
+        if (obj.rightPressed === pressed) return;
+        obj.rightPressed = pressed;
+        obj.dx = (obj.leftPressed ? -1 : 0) + (obj.rightPressed ? 1 : 0);
+        if (obj.onchange !== undefined) obj.onchange();
     };
 
     this.down = function(obj, pressed) {
-        obj.dy = pressed ? 1 : 0;
+        if (obj.downPressed === pressed) return;
+        obj.downPressed = pressed;
+        obj.dy = (obj.upPressed ? -1 : 0) + (obj.downPressed ? 1 : 0);
+        if (obj.onchange !== undefined) obj.onchange();
     };
 
     this.attach = function(element) {
         var $this = this;
         element.addEventListener('keydown', function(e) {
-            var u = $this[$this.keyMap[e.keyCode]];
+            var u = $this.keyMap[e.keyCode];
             if (u !== undefined) {
                 u($this, true);
                 e.cancelBubble = true;
@@ -52,7 +94,7 @@ yoob.Joystick = function() {
             }
         }, true);
         element.addEventListener('keyup', function(e) {
-            var u = $this[$this.keyMap[e.keyCode]];
+            var u = $this.keyMap[e.keyCode];
             if (u !== undefined) {
                 u($this, false);
                 e.cancelBubble = true;
