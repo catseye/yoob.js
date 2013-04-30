26af3e030796f48b49186c00aaf87fa2fb38b1f9
catseye <cpressey@gmail.com>
2013-04-30 13:02:27 +0100

    Add initial version of yoob.Chargen, and demo for same.

---
+++ src/yoob/chargen.js
@@ -0,0 +1,123 @@
+/*
+ * This file is part of yoob.js version 0.5-PRE
+ * Available from https://github.com/catseye/yoob.js/
+ * This file is in the public domain.  See http://unlicense.org/ for details.
+ */
+if (window.yoob === undefined) yoob = {};
+
+/*
+ * yoob.Chargen is an object for producing bitmapped-character-like displays,
+ * such as those found on 8-bit retrocomputers.  A monochromatic image, with
+ * the character patterns in a regular grid, must be supplied.  Chromatic
+ * versions of the character patterns, in each of the given colours, will be
+ * automatically created.
+ *
+ * The character data may also be modified programatically.
+ *
+ * There may be better ways to accomplish this feat -- perhaps far, far better
+ * ways -- but this aims to be reasonable and good-enough-for-now.
+ */
+yoob.Chargen = function() {
+    this.init = function(colourTriples, charWidth, charHeight,
+                         charsPerRow, rows, imageSrc, onLoad) {
+        this.colourTriples = colourTriples;
+        this.bitmaps = [];  // canvas elements
+
+        for (var i = 0; i < colourTriples.length; i++) {
+            var canvas = document.createElement("canvas");
+            canvas.width = charWidth * charsPerRow;
+            canvas.height = charHeight * rows;
+            this.bitmaps.push(canvas);
+        }
+
+        this.charWidth = charWidth;
+        this.charHeight = charHeight;
+        this.charsPerRow = charsPerRow;
+        this.rows = rows;
+        this.img = new Image();
+        var $this = this;
+        this.img.onload = function() {
+            $this.reset();
+            onLoad();
+        }
+        this.img.src = imageSrc;
+        return this;
+    };
+
+    /*
+     * Copy the image of a character from this Chargen into a given
+     * drawing context.  x and y are canvas positions of the destination.
+     */
+    this.blitChar = function(charNum, charColour, ctx, x, y,
+                             destWidth, destHeight) {
+        destWidth = destWidth || this.charWidth;
+        destHeight = destHeight || this.charHeight;
+        var srcCharX = (charNum % this.charsPerRow) * this.charWidth;
+        var srcCharY = Math.floor(charNum / this.charsPerRow) * this.charHeight;
+        ctx.drawImage(this.bitmaps[charColour],
+          /* source */ srcCharX, srcCharY, this.charWidth, this.charHeight,
+          /* dest   */ x, y, destWidth, destHeight);
+    };
+
+    this.reset = function() {
+        var ctx = this.bitmaps[0].getContext('2d');
+        var w = this.img.width;
+        var h = this.img.height;
+        ctx.clearRect(0, 0, w, h);
+        ctx.drawImage(this.img, 0, 0, w, h);
+        this.createChromaticBitmaps();
+    };
+
+    /*
+     * Probably you will never need to call this directly.
+     */
+    this.createChromaticBitmaps = function() {
+        var ctx = this.bitmaps[0].getContext('2d');
+        var w = this.img.width;
+        var h = this.img.height;
+        var imageData = ctx.getImageData(0, 0, w, h);
+        for (var color = 1; color < this.colourTriples.length; color++) {
+            var newData = ctx.getImageData(0, 0, w, h);
+            for (var y = 0; y < h; y++) {
+                for (var x = 0; x < w; x++) {
+                    var index = (y * w + x) * 4;
+                    var red = imageData.data[index];
+                    var green = imageData.data[index + 1];
+                    var blue = imageData.data[index + 2];
+                    var alpha = imageData.data[index + 3];
+                    newData.data[index] = this.colourTriples[color][0];
+                    newData.data[index + 1] = this.colourTriples[color][1];
+                    newData.data[index + 2] = this.colourTriples[color][2];
+                    newData.data[index + 3] = alpha;
+                }
+            }
+            this.bitmaps[color].getContext('2d').putImageData(newData, 0, 0);
+        }
+    };
+
+    this.modifyChar = function(charNum, bytes) {
+        var w = this.charWidth;
+        var h = this.charHeight;
+        var srcCharX = (charNum % this.charsPerRow) * w;
+        var srcCharY = Math.floor(charNum / this.charsPerRow) * h;
+        var ctx = this.bitmaps[0].getContext('2d');
+        var newData = ctx.createImageData(w, h);
+        for (var y = 0; y < h; y++) {
+            var byt = bytes[y];
+            for (var x = w - 1; x >= 0; x--) {
+                var index = (y * w + x) * 4;
+                if (byt & 1 === 1) {
+                    newData.data[index] = 0;
+                    newData.data[index + 1] = 0;
+                    newData.data[index + 2] = 0;
+                    newData.data[index + 3] = 255;
+                }
+                byt >>= 1;
+            }
+        }
+        ctx.clearRect(srcCharX, srcCharY, w, h);
+        ctx.putImageData(newData, srcCharX, srcCharY);
+        // overkill!!!
+        this.createChromaticBitmaps();
+    };
+};
