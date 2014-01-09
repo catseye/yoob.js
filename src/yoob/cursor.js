385b1527d21b10187e07e1851b0c719821fd626f
Cat's Eye Technologies <cpressey@catseye.tc>
2014-01-09 22:30:32 +0000

    Cursor by default renders itself the same way on canvas and HTML.

--- src/yoob/cursor.js
+++ src/yoob/cursor.js
@@ -1,5 +1,5 @@
 /*
- * This file is part of yoob.js version 0.3
+ * This file is part of yoob.js version 0.6-PRE
  * Available from https://github.com/catseye/yoob.js/
  * This file is in the public domain.  See http://unlicense.org/ for details.
  */
@@ -9,6 +9,12 @@ if (window.yoob === undefined) yoob = {};
  * An object representing a pointer (position vector) into two-dimensional
  * Cartesian space (possibly a yoob.Playfield) with a direction vector
  * (that need not be used).
+ *
+ * A cursor contains a built-in simple view, i.e. it knows how to render
+ * itself on a canvas (drawContext method) or in the midst of HTML text
+ * (wrapText method).  These methods are used by the playfield view classes.
+ * The supplied methods draw basic block cursors in the colour given by
+ * the fillStyle attribute, if present, or a light green if it is not set.
  */
 yoob.Cursor = function(x, y, dx, dy) {
     this.x = x;
@@ -65,8 +71,20 @@ yoob.Cursor = function(x, y, dx, dy) {
         }
     };
 
+    /*
+     * For HTML views.  Override if you like.
+     */
+    this.wrapText = function(text) {
+        var fillStyle = this.fillStyle || "#50ff50";
+        return '<span style="background: ' + this.fillStyle + '">' +
+               text + '</span>';
+    };
+
+    /*
+     * For canvas views.  Override if you like.
+     */
     this.drawContext = function(ctx, x, y, cellWidth, cellHeight) {
-        ctx.fillStyle = "#50ff50";
+        ctx.fillStyle = this.fillStyle || "#50ff50";
         ctx.fillRect(x, y, cellWidth, cellHeight);
     };
 }
