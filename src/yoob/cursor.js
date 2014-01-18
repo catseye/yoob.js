66336f705199baf972810dd31ec96a7fc795e24e
Cat's Eye Technologies <cpressey@catseye.tc>
2014-01-18 12:06:24 +0000

    Deprecate yoob.TapeHead.  Use yoob.Cursor instead!

--- src/yoob/cursor.js
+++ src/yoob/cursor.js
@@ -6,21 +6,40 @@
 if (window.yoob === undefined) yoob = {};
 
 /*
- * An object representing a pointer (position vector) into two-dimensional
- * Cartesian space (possibly a yoob.Playfield) with a direction vector
- * (that need not be used).
+ * An object representing a position and direction in some space.  The space
+ * may be one-dimensional (a yoob.Tape, or a string representing a program
+ * source) or a two-dimensional Cartesian space (such as a yoob.Playfield.)
+ * A direction vector accompanies the position, so the cursor can "know which
+ * way it's headed", but this facility need not be used.
  *
  * A cursor contains a built-in simple view, i.e. it knows how to render
  * itself on a canvas (drawContext method) or in the midst of HTML text
- * (wrapText method).  These methods are used by the playfield view classes.
- * The supplied methods draw basic block cursors in the colour given by
- * the fillStyle attribute, if present, or a light green if it is not set.
+ * (wrapText method).  These methods are used by the view classes (playfield,
+ * tape, source, etc.)  The default methods draw basic block cursors in the
+ * colour given by the fillStyle attribute, if present, or a light green if
+ * it is not defined.
  */
 yoob.Cursor = function(x, y, dx, dy) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
+
+    this.getX = function() {
+        return this.x;
+    };
+
+    this.getY = function() {
+        return this.y;
+    };
+
+    this.setX = function(x) {
+        this.x = x;
+    };
+
+    this.setY = function(y) {
+        this.y = y;
+    };
 
     this.isHeaded = function(dx, dy) {
         return this.dx === dx && this.dy === dy;
@@ -69,6 +88,31 @@ yoob.Cursor = function(x, y, dx, dy) {
         } else if (this.dx === 1 && this.dy === -1) {
             this.dx = 0; this.dy = -1;
         }
+    };
+
+    /* from yoob.TapeHead; may go away or change slightly */
+    this.move = function(delta) {
+        this.x += delta;
+    };
+
+    this.moveLeft = function(amount) {
+        if (amount === undefined) amount = 1;
+        this.x -= amount;
+    };
+
+    this.moveRight = function(amount) {
+        if (amount === undefined) amount = 1;
+        this.x += amount;
+    };
+
+    this.read = function() {
+        if (!this.tape) return undefined;
+        return this.tape.get(this.x);
+    };
+
+    this.write = function(value) {
+        if (!this.tape) return;
+        this.tape.put(this.x, value);
     };
 
     /*
