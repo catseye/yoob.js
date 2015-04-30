cda6d0700a0ef85130c4e4084d306ea65678c8b9
Chris Pressey <cpressey@catseye.tc>
2015-04-30 08:33:49 +0100

    Make yoob.Chargen take an init object, fix up small things in it.

--- src/yoob/chargen.js
+++ src/yoob/chargen.js
@@ -1,5 +1,5 @@
 /*
- * This file is part of yoob.js version 0.5
+ * This file is part of yoob.js version 0.9-PRE
  * Available from https://github.com/catseye/yoob.js/
  * This file is in the public domain.  See http://unlicense.org/ for details.
  */
@@ -7,8 +7,8 @@ if (window.yoob === undefined) yoob = {};
 
 /*
  * yoob.Chargen is an object for producing bitmapped-character-like displays,
- * such as those found on 8-bit retrocomputers.  A monochromatic image, with
- * the character patterns in a regular grid, must be supplied.  Chromatic
+ * such as those found on 8-bit home computers of olde.  A monochromatic image,
+ * with the character patterns in a regular grid, must be supplied.  Chromatic
  * versions of the character patterns, in each of the given colours, will be
  * automatically created.
  *
@@ -18,29 +18,28 @@ if (window.yoob === undefined) yoob = {};
  * ways -- but this aims to be reasonable and good-enough-for-now.
  */
 yoob.Chargen = function() {
-    this.init = function(colourTriples, charWidth, charHeight,
-                         charsPerRow, rows, imageSrc, onLoad) {
-        this.colourTriples = colourTriples;
+    this.init = function(cfg) {
+        this.colorTriples = cfg.colorTriples || [[0, 0, 0], [255, 255, 255]];
+        this.charWidth = cfg.charWidth || 8;
+        this.charHeight = cfg.charHeight || 8;
+        this.charsPerRow = cfg.charsPerRow || 16;
+        this.rows = cfg.rows || 16;
         this.bitmaps = [];  // canvas elements
 
-        for (var i = 0; i < colourTriples.length; i++) {
+        for (var i = 0; i < this.colorTriples.length; i++) {
             var canvas = document.createElement("canvas");
-            canvas.width = charWidth * charsPerRow;
-            canvas.height = charHeight * rows;
+            canvas.width = this.charWidth * this.charsPerRow;
+            canvas.height = this.charHeight * this.rows;
             this.bitmaps.push(canvas);
         }
 
-        this.charWidth = charWidth;
-        this.charHeight = charHeight;
-        this.charsPerRow = charsPerRow;
-        this.rows = rows;
         this.img = new Image();
         var $this = this;
         this.img.onload = function() {
             $this.reset();
-            onLoad();
+            if (cfg.onLoad) cfg.onLoad();
         }
-        this.img.src = imageSrc;
+        this.img.src = cfg.imageSrc;
         return this;
     };
 
@@ -48,13 +47,13 @@ yoob.Chargen = function() {
      * Copy the image of a character from this Chargen into a given
      * drawing context.  x and y are canvas positions of the destination.
      */
-    this.blitChar = function(charNum, charColour, ctx, x, y,
+    this.blitChar = function(charNum, charColor, ctx, x, y,
                              destWidth, destHeight) {
         destWidth = destWidth || this.charWidth;
         destHeight = destHeight || this.charHeight;
         var srcCharX = (charNum % this.charsPerRow) * this.charWidth;
         var srcCharY = Math.floor(charNum / this.charsPerRow) * this.charHeight;
-        ctx.drawImage(this.bitmaps[charColour],
+        ctx.drawImage(this.bitmaps[charColor],
           /* source */ srcCharX, srcCharY, this.charWidth, this.charHeight,
           /* dest   */ x, y, destWidth, destHeight);
     };
@@ -65,29 +64,29 @@ yoob.Chargen = function() {
         var h = this.img.height;
         ctx.clearRect(0, 0, w, h);
         ctx.drawImage(this.img, 0, 0, w, h);
-        this.createChromaticBitmaps();
+        this._createChromaticBitmaps();
     };
 
     /*
      * Probably you will never need to call this directly.
      */
-    this.createChromaticBitmaps = function() {
+    this._createChromaticBitmaps = function() {
         var ctx = this.bitmaps[0].getContext('2d');
         var w = this.img.width;
         var h = this.img.height;
         var imageData = ctx.getImageData(0, 0, w, h);
-        for (var color = 1; color < this.colourTriples.length; color++) {
+        for (var color = 1; color < this.colorTriples.length; color++) {
             var newData = ctx.getImageData(0, 0, w, h);
             for (var y = 0; y < h; y++) {
                 for (var x = 0; x < w; x++) {
                     var index = (y * w + x) * 4;
-                    var red = imageData.data[index];
-                    var green = imageData.data[index + 1];
-                    var blue = imageData.data[index + 2];
+                    //var red = imageData.data[index];
+                    //var green = imageData.data[index + 1];
+                    //var blue = imageData.data[index + 2];
                     var alpha = imageData.data[index + 3];
-                    newData.data[index] = this.colourTriples[color][0];
-                    newData.data[index + 1] = this.colourTriples[color][1];
-                    newData.data[index + 2] = this.colourTriples[color][2];
+                    newData.data[index] = this.colorTriples[color][0];
+                    newData.data[index + 1] = this.colorTriples[color][1];
+                    newData.data[index + 2] = this.colorTriples[color][2];
                     newData.data[index + 3] = alpha;
                 }
             }
@@ -117,7 +116,7 @@ yoob.Chargen = function() {
         }
         ctx.clearRect(srcCharX, srcCharY, w, h);
         ctx.putImageData(newData, srcCharX, srcCharY);
-        // overkill!!!
-        this.createChromaticBitmaps();
+        // FIXME this is such overkill!!!
+        this._createChromaticBitmaps();
     };
 };
