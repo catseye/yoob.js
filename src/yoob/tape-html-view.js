f53db1db78ab7d83de6bd1508ed780f92e1aed36
Chris Pressey <cpressey@catseye.tc>
2015-05-10 21:01:43 +0100

    Add initial versions of StackHtmlView and TapeHtmlView.

---
+++ src/yoob/tape-html-view.js
@@ -0,0 +1,53 @@
+/*
+ * This file is part of yoob.js version 0.10-PRE
+ * Available from https://github.com/catseye/yoob.js/
+ * This file is in the public domain.  See http://unlicense.org/ for details.
+ */
+if (window.yoob === undefined) yoob = {};
+
+/*
+ * A view (in the MVC sense) for depicting a yoob.Tape (-compatible)
+ * object onto any DOM element that supports innerHTML.
+ */
+yoob.TapeHTMLView = function() {
+    this.init = function(cfg) {
+        this.tape = cfg.tape;
+        this.element = cfg.element;
+        return this;
+    };
+
+    /*** Chainable setters ***/
+
+    this.setTape = function(tape) {
+        this.tape = tape;
+        return this;
+    };
+
+    /*
+     * For compatibility with PlayfieldCanvasView.  Sets the font size.
+     */
+    this.setCellDimensions = function(cellWidth, cellHeight) {
+        this.element.style.fontSize = cellHeight + "px";
+        return this;
+    };
+
+    /*
+     * Convert Tape values to HTML.  Override to customize appearance.
+     */
+    this.render = function(value) {
+        if (value === undefined) return ' ';
+        return value;
+    };
+
+    /*
+     * Render the Tape, as HTML, on the DOM element.
+     * TODO: make this not awful.
+     */
+    this.draw = function() {
+        var text = "";
+        this.tape.foreach(function(pos, value) {
+            text += '' + pos + ': ' + this.render(value) + "<br/>";
+        }
+        this.element.innerHTML = text;
+    };
+};
